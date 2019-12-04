686b464402b6f0a22ac1c73e6a1ad98a08cc6a88
omgwaagainwtf <steven850507@gmail.com>
2019-12-05 04:10:53 +0800

    fixed total page in tags-search

--- tags_search.py
+++ tags_search.py
@@ -3,12 +3,12 @@ import json
 import math
 def tags_search(cur_category,cur_page):
     db_name = "category_tag"
-    res = gp.Select(db_name,pattern={'col':['category'],'val':[cur_category]},ret_col=['keywords'],page_cnt=100,page=cur_page)
+    res = gp.Select(db_name,pattern={'col':['category'],'val':[cur_category]},ret_col=['keywords'],page_cnt=10,page=cur_page)
     ret = json.loads(res)
     ret = json.loads(ret['data'])
     tags = []
     if ret['cnt'] > 0:
         tags = ret['recs'][0]['rec']['keywords'].split(',')
-    total_page = math.ceil(ret['cnt']/100)
+    total_page = math.ceil(len(tags)/10)
     return {"tags":tags,"pages":total_page}
 print(tags_search('政治',1))
