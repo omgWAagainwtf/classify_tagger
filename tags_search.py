2b34fc14326c8a25e3b28c6b7e7c30ca019fb1e1
omgwaagainwtf <steven850507@gmail.com>
2019-12-05 04:01:55 +0800

    fixed tags from string to list

--- tags_search.py
+++ tags_search.py
@@ -1,14 +1,14 @@
 import gaipy as gp
 import json
 import math
-def search(cur_category,cur_page):
+def tags_search(cur_category,cur_page):
     db_name = "category_tag"
     res = gp.Select(db_name,pattern={'col':['category'],'val':[cur_category]},ret_col=['keywords'],page_cnt=100,page=cur_page)
     ret = json.loads(res)
     ret = json.loads(ret['data'])
     tags = []
-    for i in range(len(ret['recs'])):
-        tags.append(ret['recs'][i]['rec']['keywords'])
-    total_page = math.ceil(ret['cnt']/10)
+    if ret['cnt'] > 0:
+        tags = ret['recs'][0]['rec']['keywords'].split(',')
+    total_page = math.ceil(ret['cnt']/100)
     return {"tags":tags,"pages":total_page}
-print(search('政治',1))
+print(tags_search('政治',1))
