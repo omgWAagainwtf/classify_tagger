76afe31341fe11af4dee6d792fb9c8e105a01a78
omgwaagainwtf <steven850507@gmail.com>
2019-12-05 01:54:51 +0800

    add pages to search and tags_search

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import gaipy as gp
 import json
+import math
 def search(cur_category,cur_page):
     db_name = "doc_tag"
     res = gp.Select(db=db_name,pattern={'col':['category'],'val':[cur_category]},filter_args={'col':["confirm"],"val":["0"]},page=cur_page,page_cnt=10)
@@ -14,5 +15,7 @@ def search(cur_category,cur_page):
         keywords.append(ret['recs'][i]['rec']['keywords'].split(','))
         category.append(ret['recs'][i]['rec']['category'])
         text_id.append(ret['recs'][i]['rec']['text_id'])
-    return {"text":document,"tag":keywords,"text_id":text_id,"category":category}
+
+    total_page = math.ceil(ret['cnt']/10)
+    return {"text":document,"tag":keywords,"text_id":text_id,"category":category,"pages":total_page}
 print(search('政治',1))
--- tags_search.py
+++ tags_search.py
@@ -1,12 +1,14 @@
 import gaipy as gp
 import json
+import math
 def search(cur_category,cur_page):
     db_name = "category_tag"
-    res = gp.Select(db_name,pattern={'col':['category'],'val':[cur_category]},ret_col=['keywords'])
+    res = gp.Select(db_name,pattern={'col':['category'],'val':[cur_category]},ret_col=['keywords'],page_cnt=100,page=cur_page)
     ret = json.loads(res)
     ret = json.loads(ret['data'])
     tags = []
     for i in range(len(ret['recs'])):
         tags.append(ret['recs'][i]['rec']['keywords'])
-    return tags
+    total_page = math.ceil(ret['cnt']/10)
+    return {"tags":tags,"pages":total_page}
 print(search('政治',1))
